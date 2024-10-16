a558855b85cd9bd9054abe9d5472c23f4b6d37bc
Pingfan Hu <pingfan0727@gmail.com>
2024-10-15 21:26:01 -0400

    update with new show_if conditions

--- app.R
+++ app.R
@@ -25,9 +25,20 @@ server <- function(input, output, session) {
 
   # Define any conditional display logic here (show a question if a condition is true)
   sd_show_if(
+
+    # Simple Conditional Display
     input$penguins_simple == "other" ~ "penguins_simple_other",
+
+    # Complex Conditional Display
     input$penguins_complex == "other" &
-      input$show_other == "show" ~ "penguins_complex_other"
+      input$show_other == "show" ~ "penguins_complex_other",
+
+    # Conditional display based on a numeric value
+    as.numeric(input$car_number) > 1 ~ "ev_ownership",
+
+    # Conditional display based on multiple inputs
+    input$fav_fruits %in% c("apple", "banana") ~ "apple_or_banana",
+    length(input$fav_fruits) > 3 ~ "fruit_number"
   )
 
   # Database designation and other settings
